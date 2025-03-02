8ca4ecab0bf3fd4f217a0037ab03c0b02c5f2f3e
Pratik-Verma-13 <pratkverma13incredibles@gmail.com>
2025-03-02 14:19:25 +0530

    responsive changes

---
+++ script.js
@@ -0,0 +1,8 @@
+menuDisplay = document.getElementById("menu-mobile-parent")
+function showMobileMenu(){
+    menuDisplay.style.display = "block"
+}
+
+function hideMobileMenu(){
+    menuDisplay.style.display = "None"
+}
