a0625ec25d4baa4caba06665fba23fee8a6b41e1
Pratik-Verma-13 <pratkverma13incredibles@gmail.com>
2025-03-04 16:55:09 +0530

    animation effect

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ function showMobileMenu(){
 function hideMobileMenu(){
     menuDisplay.style.display = "None"
 }
+
+let index = 0;
+showSlides();
+
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("card-img");
+  let text = document.getElementsByClassName("card-text");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < slides.length; i++) {
+    text[i].style.display = "none";  
+  }
+  index++;
+  if (index > slides.length) {index = 1}    
+  slides[index-1].style.display = "block";  
+  text[index-1].style.display = "flex";
+  setTimeout(showSlides, 2000); 
+}
